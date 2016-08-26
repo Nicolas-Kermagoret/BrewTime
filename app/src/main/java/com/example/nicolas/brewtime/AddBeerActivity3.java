9ea402a9b1016eb84b20ba0f550eb833ac6413de
nicolas <n2kermag@enib.fr>
2016-08-26 17:15:44 +0200

    AddBeerActivity2 is complete and launch AddBeerActivity3

---
+++ app/src/main/java/com/example/nicolas/brewtime/AddBeerActivity3.java
@@ -0,0 +1,30 @@
+package com.example.nicolas.brewtime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+/**
+ * Created by nicolas on 26/08/16.
+ */
+public class AddBeerActivity3 extends AppCompatActivity {
+
+    Beer beer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_beer_activity_3);
+
+        this.beer = (Beer)this.getIntent().getSerializableExtra("Beer");
+
+        this.setSupportActionBar((Toolbar)this.findViewById(R.id.toolbar));
+        this.getSupportActionBar().setTitle("Ingredients");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.validation_bar, menu);
+        return true;
+    }
+}
